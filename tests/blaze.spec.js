bae866eca99d12de8867a64c11a2a0cfc4715508
noyonsarder <noyonsarder87@gmail.com>
2025-02-22 01:04:30 +0600

    QA-5: Added the capability run test cases based on the test suite selection

--- tests/blaze.spec.js
+++ tests/blaze.spec.js
@@ -1,17 +1,12 @@
 const {test, expect} = require('@playwright/test');
 
-
 test.describe("@regression", ()=>{
-
     test("First Test Case", async({page})=>{
-
         await page.goto("https://demoblaze.com/index.html");
         const title= await page.title();
         await expect(title).toContain("STORE");
     })
-    
     test("Second Test Case", async({page})=>{
-    
         await page.goto("https://demoblaze.com/index.html");
         const url= await page.url();
         await expect(url).toBe("https://demoblaze.com/index.html");
@@ -20,9 +15,7 @@ test.describe("@regression", ()=>{
 })
 
 test.describe("smoke",()=>{
-
     test("Third Test Case", async({page})=>{
-
         await page.goto("https://demoblaze.com/index.html");
         await page.waitForSelector(`//a[@id='cat']`);
         await expect(page.locator(`//a[@id='cat']`)).toBeVisible();
