48b81933ab731d11f192097bfd69f6dc5e6191ee
noyonsarder <noyonsarder87@gmail.com>
2025-02-22 00:33:10 +0600

    QA-4: Added Group for filtering test cases

--- tests/blaze.spec.js
+++ tests/blaze.spec.js
@@ -1,8 +1,31 @@
 const {test, expect} = require('@playwright/test');
 
-test("First Test Case", async({page})=>{
 
-    await page.goto("https://demoblaze.com/index.html");
-    const title= await page.title();
-    await expect(title).toContain("STORE");
+test.describe("@regression", ()=>{
+
+    test("First Test Case", async({page})=>{
+
+        await page.goto("https://demoblaze.com/index.html");
+        const title= await page.title();
+        await expect(title).toContain("STORE");
+    })
+    
+    test("Second Test Case", async({page})=>{
+    
+        await page.goto("https://demoblaze.com/index.html");
+        const url= await page.url();
+        await expect(url).toBe("https://demoblaze.com/index.html");
+    
+    })
+})
+
+test.describe("smoke",()=>{
+
+    test("Third Test Case", async({page})=>{
+
+        await page.goto("https://demoblaze.com/index.html");
+        await page.waitForSelector(`//a[@id='cat']`);
+        await expect(page.locator(`//a[@id='cat']`)).toBeVisible();
+        
+    })
 })
